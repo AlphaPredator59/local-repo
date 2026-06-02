1a8c577732ab8212a3529eb1246d2d127595d17d
Parv Baj <parvbaj02@gmail.com>
2026-06-02 11:15:53 +0530

    checking for merge conflicts

--- golang.go
+++ golang.go
@@ -2,5 +2,5 @@ package main
 import ("fmt")
 func main(){
 	fmt.Println("This is for creating a new repo locally from my laptop")
-	fmt.Println("Adding feature 1 branch")
+	fmt.Println("Adding feature 1 branch and using it")
 }
